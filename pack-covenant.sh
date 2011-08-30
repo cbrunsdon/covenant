7b9c9c532a1bad11c4f9c9bcb181536ce3c25668
Clarke Brunsdon <clarke@freerunningtechnologies.com>
2011-08-30 04:03:11 -0700

    A first quick version to get me off of the reddits. And yes, I'm checking in the package for easy distrobution. <3 you github

---
+++ pack-covenant.sh
@@ -0,0 +1,2 @@
+#!/bin/zsh
+crxmake --pack-extension=src --extension-output=package/covenant.crx --pack-extension-key=covenant.pem
---
+++ src/covenant_infringement.js
@@ -0,0 +1,1 @@
+window.location = chrome.extension.getURL("main.html");
